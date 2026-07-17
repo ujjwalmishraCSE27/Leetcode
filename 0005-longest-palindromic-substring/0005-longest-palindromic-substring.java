4389ade75bc647a8f273208c1b623200f1e8a03a
Ujjwal Mishra <ujjwalmishracse27@gmail.com>
2026-07-17 12:16:32 +0530

    Time: 152 ms (23.71%), Space: 95.8 MB (5.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,43 @@
 class Solution {
-    int dp[][]=new int[1001][1001];
-    public boolean check(String s,int st,int e){
-      if(st>=e) return true;
-      if(dp[st][e]!=-1){
-        if(dp[st][e]==0) return false;
-        else return true;
-      }
-      if(s.charAt(st)==s.charAt(e) && check(s,st+1,e-1)){
-         dp[st][e]=1;
-         return true;
-      }
-      dp[st][e]=0;
-      return false;
-    }
     public String longestPalindrome(String s) {
-        int ans=0;
-        for(int a[]:dp){
-            Arrays.fill(a,-1);
-        }
+        int ans=-1,maxL=0;
+        int n=s.length();
+        int len=0;
+        if(n==1 || s.equals("")) return s;
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(check(s,i,j)){
-                    if(j-i+1>ans){
+        int dp[][]=new int[n+1][n+1];
+        for(int l=1;l<=s.length();l++){
+            for(int i=0;i+l-1<n;i++){
+                 int j=i+l-1;
+                 if(l==1){
+                    dp[i][j]=1;
+                    if(l>len){
                         sb.setLength(0);
                         sb.append(s,i,j+1);
-                        ans=j-i+1;
+                        len=l;
                     }
-
-                }
+                 }
+                 if(l==2 && s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=1;
+                    if(l>len){
+                        sb.setLength(0);
+                        sb.append(s,i,j+1);
+                        len=l;
+                    }
+                 }
+                 if(l>=3){
+                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==1)
+                     { dp[i][j]=1;
+                       if(l>len){
+                        sb.setLength(0);
+                        sb.append(s,i,j+1);
+                        len=l;
+                    }
+                }  
+                 }
             }
         }
         return sb.toString();
+        
     }
 }

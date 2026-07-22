6fa8a8af519fab861d9880cdc9ff59ee5c77099e
Ujjwal Mishra <ujjwalmishracse27@gmail.com>
2026-07-22 16:58:48 +0530

    Time: 1 ms (14.69%), Space: 42.1 MB (48.31%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,22 @@
 class Solution {
-    int dp[][]=new int[101][101];
-    public int solve(int i,int j,int m,int n){
-        if(i<0 || i>m || j<0 || j>n) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int right=solve(i,j+1,m,n);
-        int down=solve(i+1,j,m,n);
-        return dp[i][j]=right+down;
-    }
     public int uniquePaths(int m, int n) {
-        for(int a[]:dp){
-            Arrays.fill(a,-1);
+        if(m==1 && n==1) return 1;
+        int dp[][]=new int[m+1][n+1];
+        int ans=0;
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 && j==0) dp[i][j]=0;
+                else if(i==0) dp[i][j]=1;
+                else if(j==0) dp[i][j]=1;
+            }
         }
-        return solve(0,0,m,n);
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            
+            }
+        }
+        return dp[m-1][n-1];
     }
 }

d11d1067c4383eccd825979bce4e18a7656de2e3
Ujjwal Mishra <ujjwalmishracse27@gmail.com>
2026-08-18 20:51:28 +0530

    Time: 3 ms (29.99%), Space: 45.5 MB (21.1%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,27 +1,34 @@
 class Solution {
-    public int dp[][];
-    public int solve(int arr[][],int i,int j){
-        int res1=0,res=0;
-        if(i==arr.length || j==arr[0].length){
-            return Integer.MAX_VALUE/2;
-        }
-        if(i==arr.length-1 && j==arr[0].length-1){
-            if(arr[i][j]<=0)
-               return Math.abs(arr[i][j])+1;
-            else return 1;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down=solve(arr,i+1,j);
-        int right=solve(arr,i,j+1);
-        res=Math.min(down,right)-arr[i][j];
-        if(res<=0) return dp[i][j]=1;
-        else return dp[i][j]=res;
-    }
     public int calculateMinimumHP(int[][]arr) {
-        dp=new int[arr.length][arr[0].length];
-        for(int a[]:dp){
-            Arrays.fill(a,-1);
+        int n=arr.length;
+        int m=arr[0].length;
+        int dp[][]=new int[arr.length][arr[0].length];
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1) 
+                {if(arr[i][j]<=0) dp[n-1][m-1]=Math.abs(arr[n-1][m-1])+1;
+                    else dp[n-1][m-1]=1;
+                    continue;
+                }
+                int res=0;
+                int down=Integer.MAX_VALUE/2;
+                int right=Integer.MAX_VALUE/2;
+                if(j==m-1 && i!=n-1){
+                    down=dp[i+1][j];
+                }
+                else if(i==n-1 && j!=m-1){
+                    right=dp[i][j+1];
+                }
+                else if(i!=n-1 && j!=m-1){
+                    down=down=dp[i+1][j];
+                    right=right=dp[i][j+1];
+                }
+                res=Math.min(down,right)-arr[i][j];
+                if(res<=0) dp[i][j]=1;
+                else dp[i][j]=res;
+            }
         }
-        return solve(arr,0,0);
+        return dp[0][0];
     }
 }

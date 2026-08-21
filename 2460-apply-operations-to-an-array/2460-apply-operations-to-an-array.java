04e6b5f5914ec17549013fbff5ac47a5322fc8eb
Ujjwal Mishra <ujjwalmishracse27@gmail.com>
2026-08-21 22:19:38 +0530

    Time: 1 ms (96.14%), Space: 45.3 MB (43.5%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[idx]=nums[i];
+                idx++;
+            }
+        }
+        while(idx<nums.length){
+            nums[idx]=0;
+            idx++;
+        }
+        return nums;
+    }
+}
